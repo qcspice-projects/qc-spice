c66b3572de83b191c2b4341975045283f9ec999c
Stephanie McHugh <Stephanie.M.McHugh@ibm.com>
2021-11-24 12:47:18 -0500

    header button, beginning of dblClick

--- pages/components/QubitDraggable/QubitDraggable.tsx
+++ pages/components/QubitDraggable/QubitDraggable.tsx
@@ -1,4 +1,5 @@
-import { DragEvent } from "react";
+import { DragEvent, MouseEvent, useState } from "react";
+import styles from "./QubitDraggable.module.scss";
 
 interface QubitProps {
   compType: string;
@@ -7,17 +8,23 @@ interface QubitProps {
   dragStart: (event: DragEvent, nodeType: string) => void;
 }
 
+// TODO: needs to be a CustomNode type for ReactFlow
 export const QubitComponent = (props: QubitProps) => {
   // if (!props.isSubsystem)
+  // const [valueFieldVisible, setValueFieldVisible] = useState(false);
+
+  // TODO: would single click or double click be better for this?
+  // const onDblClick = (event: MouseEvent) => setValueFieldVisible(true);
   return (
     <>
       <div
-        className="dndnode"
+        className={"dndnode " + styles.qubitComponent}
         draggable
         onDragStart={(event: DragEvent) => {
           console.log("drag start");
           props.dragStart(event, props.compType);
         }}
+        // onDoubleClick={(event: MouseEvent) => onDblClick(event)}
       >
         {props.compName}
       </div>
